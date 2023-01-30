d41166fd225667ece66202ee2443e6bee69db534
bmigo <berniecmigo@gmail.com>
2023-01-30 13:23:48 -0500

    more javascript for quiz

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var timer = document.querySelector(".timer");
 var startButton = document.querySelector(".startQuiz");
-var quizBox = document.querySelector(".questions");
+var quizBox = document.querySelector(".answers");
+var quizQuestion = document.querySelector(".questions");
 
 var secondsLeft = 75;
 var deduction = 5;
@@ -34,25 +35,43 @@ var questions = [
     {
         q:"HELLOOOO",
         choices: ["A. BEE", "B. ", "C. ", "D. "],
-        a:"",
+        a:"B. ",
     },
+    {
+        q:"HELLOOOO",
+        choices: ["A. BEE", "B. ", "C. ", "D. "],
+        a:"B. ",
+    },
+
 ]
 
 function playGame () {
     quizBox.textContent = "";
     for (var i=0; i<questions.length; i++){
         var gameQuestion = questions[questionsIndex].q;
-        var choiceA = questions[questionsIndex].choices[0];
-        var choiceB = questions[questionsIndex].choices[1];
-        var choiceC = questions[questionsIndex].choices[2];
-        var choiceD = questions[questionsIndex].choices[3];
+        var gameChoices = questions[questionsIndex].choices;
 
-        quizBox.innerHTML = gameQuestion + "<br />" + 
-        choiceA + "<br />" + 
-        choiceB + "<br />" + 
-        choiceC + "<br />" + 
-        choiceD + "<br />";
-        
+        quizQuestion.innerHTML = gameQuestion + "<br />";
+
+        gameChoices.forEach(function(newChoice) {
+            var listChoice = document.createElement("p");
+            listChoice.textContent = newChoice;
+            quizBox.appendChild(listChoice);
+            listChoice.addEventListener("click", console.log("hello"));
+        });
+
+
+
+
+
+        /* choiceA + " <button type='button'>:)</button>" + "<br />" + 
+        choiceB + " <button type='button'>:)</button>" + "<br />" + 
+        choiceC + " <button type='button'>:)</button>" + "<br />" + 
+        choiceD + " <button type='button'>:)</button>" + "<br />";
+
+
+
+        button.addEventListener("click", console.log("click")); */
 
     }
 
